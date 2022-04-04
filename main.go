ea2d625b83bfba45f05d45fdb9ed78ab391ce3b6
Tim Hatch <hatch.tim@gmail.com>
2022-04-04 12:24:41 +0100

    Pare back example

--- main.go
+++ main.go
@@ -1,29 +1,40 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/gin-gonic/gin"
-	_ "github.com/heroku/x/hmetrics/onload"
 )
 
-func main() {
-	port := os.Getenv("PORT")
+type app struct{}
 
-	if port == "" {
-		log.Fatal("$PORT must be set")
+func (a *app) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	status := http.StatusOK
+	keys := r.URL.Query()["key"]
+	msg := "dunia"
+	if len(keys) > 0 {
+		msg = (keys[0])
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	body := `{"Message": "Hello ` + msg + `"}` + "\n"
+	io.WriteString(w, body)
+	log.Printf("\"%s %s %s\" %d %d\n", r.Method, r.URL.Path, r.Proto, status, len(body))
+}
 
-	router.Run(":" + port)
+func main() {
+	port, ok := os.LookupEnv("PORT")
+
+	if !ok {
+		port = "8080"
+	}
+
+	log.Printf("Starting server on port %s\n", port)
+
+	if err := http.ListenAndServe(":"+port, &app{}); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
 }
